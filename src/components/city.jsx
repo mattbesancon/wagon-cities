6506e52491dbeb829ff0047ef84611be7d110456
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-17 16:36:30 +0200

    fundamentals for components

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,17 +1,20 @@
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+
 import React from 'react';
 
 const cities = [
   { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
   { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
   { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-];
+];  
 
-const App = () => {
+const City = (props) => {
   return (
-    <div className="app">
-      <CityList cities={cities} />
+    <div className="cities">
+      {this.props.name}
     </div>
   );
 };
 
-export default App;
+export default City;
---
+++ src/components/city_list.jsx
@@ -0,0 +1,32 @@
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+
+import React from 'react';
+import City from './city';
+
+const cities = [
+  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
+  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
+  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
+];  
+
+const CityList = (props) => {
+  const renderList = () => {
+    return props.cities.map((city) => {
+      return (
+        <City
+          city={city}
+          key={city.address}
+        />
+      );
+    });
+  };
+
+  return (
+    <div className="cities">
+      {renderList()}
+    </div>
+  );
+};
+
+export default CityList;
