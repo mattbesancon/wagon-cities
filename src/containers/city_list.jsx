06cd00d15075d34449ae04d8726f94e24917c2dd
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-04-20 16:41:31 +0200

    with eslint goooooood

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -7,6 +7,7 @@ import City from './city';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { setCities } from '../actions';
+
 class CityList extends Component {
   componentWillMount() {
     this.props.setCities();
@@ -36,7 +37,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
-    { setCities: setCities },
+    { setCities },
     dispatch
   );
 }
